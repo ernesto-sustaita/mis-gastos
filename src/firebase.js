d57f19b20d0923325f1bcaf1ed50e85ad67c4e26
Ernesto Sustaita <ernesto.sustaita@yahoo.fr>
2022-01-27 22:33:42 -0600

    Initial create

---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app"
+import 'firebase/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCrPmUmHNxPw9FUkFYPaJFTQeG4xPoqJFM",
+
+  authDomain: "mis-gastos-1fb64.firebaseapp.com",
+
+  projectId: "mis-gastos-1fb64",
+
+  storageBucket: "mis-gastos-1fb64.appspot.com",
+
+  messagingSenderId: "501421441297",
+
+  appId: "1:501421441297:web:d01b953b20d20c1ab58cb0"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+//export const db = app.firestore();
