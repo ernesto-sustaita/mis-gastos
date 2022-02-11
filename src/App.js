5d42eeb5cfdec6c8b7f3b2038ca566d27af78568
Ernesto Sustaita <ernesto.sustaita@yahoo.fr>
2022-02-11 03:40:38 -0600

    Initial create

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Links from './components/Links';
-import LinkForm from './components/LinkForm';
 
 function App() {
   return (
-    <>
-      <Links/>
-      <LinkForm/>
-    </>
+    <div className='container p-4'>
+      <div className='row'>
+        <Links/>
+      </div>
+    </div>
   );
 }
 
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import LinkForm from "./LinkForm";
+
+import {db} from '../firebase';
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 
 const Links = () => {
-    return <h1>Links</h1>
-}
+
+    const [links, setLinks] = useState([]);
+
+    const addOrEditLink = async (linkObject) => {
+        const linksCollection =  collection(db,'links');
+
+        await addDoc(linksCollection,linkObject);
+        
+        console.log('new task added');
+    }
+
+    const getLinks = async () => {
+
+        onSnapshot(collection(db,'links'), (collection)=>{
+            const docs = [];
+            collection.forEach(doc => {
+                docs.push({...doc.data(), id:doc.id});
+            });
+            setLinks(docs);
+        });
+    }
+
+    useEffect(()=>{
+        getLinks();
+    },[]);
+
+    return(
+        <div>
+            <div className="col-md-4 p-2">
+                <LinkForm addOrEditLink={addOrEditLink}/>
+            </div>
+            <div className="col-md-4 p-2">
+                {links.map(link=>(
+                    <div key={link.id} className="card mb-1">
+                        <div className="card-body">
+                            <h4>{link.name}</h4>
+                            <p>{link.description}</p>
+                            <a href={link.url} target="_blank" rel="noreferrer">Go to</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Links;
